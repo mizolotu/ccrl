27726cf007619bd0c6d5588a2986b45c30e6ec62
mizolotu <38492363+mizolotu@users.noreply.github.com>
2020-01-23 12:39:47 +0200

    Update train_agents.py

--- train_agents.py
+++ train_agents.py
@@ -97,6 +97,6 @@ if __name__ == '__main__':
     print('Total time steps: {0}'.format(total_timesteps))
 
     for network in networks:
-        if 'alstm' in network:
+        if 'lstm' in network:
             print(network)
             test_alg_on_env(env_classes[0], algorithms[3], network, ne=n_envs, ns=n_steps, tt=total_timesteps)
